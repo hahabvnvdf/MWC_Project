fd9349ad010aa6607509a7a518b71a8e62bed9a3
Albert Walser <walsea@usi.ch>
2020-11-29 19:01:18 +0100

    Added location tracker to MainActivity.java

--- StepMapper/app/src/main/java/com/example/stepmapper/ui/map/MapFragment.java
+++ StepMapper/app/src/main/java/com/example/stepmapper/ui/map/MapFragment.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.stepmapper.R;
 
 public class MapFragment extends Fragment {
+    LocationTrack locationTrack;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -26,6 +32,32 @@ public class MapFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_map, container, false);
         final TextView textView = root.findViewById(R.id.text_map);
 
+        Button btn = (Button)getView().findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                locationTrack = new LocationTrack(MapFragment.this);
+
+                if (locationTrack.canGetLocation()) {
+                    double longitude = locationTrack.getLongitude();
+                    double latitude = locationTrack.getLatitude();
+                    Toast.makeText(getApplicationContext(), "Longitude:" + Double.toString(longitude) + "\nLatitude:" + Double.toString(latitude), Toast.LENGTH_SHORT).show();
+                } else {
+                    locationTrack.showSettingsAlert();
+                }
+            }
+        });
+
+
         return root;
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        locationTrack.stopListener();
+    }
+
 }
